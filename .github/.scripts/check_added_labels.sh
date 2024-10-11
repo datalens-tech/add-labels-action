cf9ead1844a674ecefac457c423d1a10b21c5fc0
flukolo4ek <askolo4ek@yandex-team.ru>
2024-10-11 16:39:32 +0300

    lets move this out to another step

--- .github/.scripts/check_added_labels.sh
+++ .github/.scripts/check_added_labels.sh
@@ -1,5 +1,15 @@
 GH_TOKEN=${GH_TOKEN}
 
-CURRENT_PR_LABELS=$(gh pr view $PR_NUMBER --repo $REPO --json labels | jq -r '.labels[].name' | tr ' ' '\n' | sort | tr '\n' ' ')
+CURRENT_PR_LABELS=$(gh pr view $PR_NUMBER --repo $REPO --json labels | jq -r '.labels[].name')
+COMPONENTS_AND_TYPES_LABELS=""
 
-echo $CURRENT_PR_LABELS
+for label in $CURRENT_PR_LABELS
+do
+    if [[ $label == *type/* || $label == *component/* ]]; then
+        COMPONENTS_AND_TYPES_LABELS+="$label "
+    fi
+done
+
+COMPONENTS_AND_TYPES_LABELS=$(echo $COMPONENTS_AND_TYPES_LABELS  | tr ' ' '\n' | sort | tr '\n' ' ')
+
+echo $COMPONENTS_AND_TYPES_LABELS

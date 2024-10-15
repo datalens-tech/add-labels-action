6025d0e62b4776fa3d6fc2b94bad1a61744cd9f4
Dmitrii Ovsyannikov <ovsds@yandex-team.ru>
2024-10-15 09:22:50 +0200

    refactor: small cleanups (#6)

---
+++ src/construct_labels.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# shellcheck source=src/common.sh
+source "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+CURRENT_LABELS=${CURRENT_LABELS}
+PR_TITLE=${PR_TITLE}
+
+CONFIG_JSON="https://raw.githubusercontent.com/datalens-tech/datalens/main/.github/workflows/scripts/changelog/changelog_config.json"
+
+CONFIG_DATA=$(curl -s "$CONFIG_JSON")
+TYPE_LABELS_PREFIX=$(echo "$CONFIG_DATA" | jq -r '.section_tags.prefix')
+COMPONENT_LABELS_PREFIX=$(echo "$CONFIG_DATA" | jq -r '.component_tags.prefix')
+
+CURRENT_LABELS=$(echo "${CURRENT_LABELS}" | grep -E "$TYPE_LABELS_PREFIX|$COMPONENT_LABELS_PREFIX" | sort)
+
+LABELS_SECTION=${PR_TITLE%%:*}
+COMMIT_TYPE=${LABELS_SECTION%%\(*}
+COMPONENT_SECTION=${LABELS_SECTION#"$COMMIT_TYPE"}
+
+LABELS_TO_ADD="$TYPE_LABELS_PREFIX$COMMIT_TYPE"
+
+if [[ -n $COMPONENT_SECTION ]]; then
+    COMMIT_COMPONENTS=$(echo "$CONFIG_DATA" | jq -r '.component_tags.tags[].id' | awk -v component_sec="$COMPONENT_SECTION" 'component_sec ~ $0' | sed "s#^#$COMPONENT_LABELS_PREFIX#")
+    LABELS_TO_ADD+="\n$COMMIT_COMPONENTS"
+fi
+
+LABELS_TO_ADD=$(echo -e "$LABELS_TO_ADD" | sort)
+LABELS_TO_DELETE=$(comm -13 <(echo "$LABELS_TO_ADD") <(echo "$CURRENT_LABELS"))
+
+set_output "labels_to_add" "$LABELS_TO_ADD"
+set_output "labels_to_delete" "$LABELS_TO_DELETE"
--- src/labels/delete_all.sh
+++ src/labels/delete_all.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
+REPO=${REPO}
+PR_NUMBER=${PR_NUMBER}
+
 gh api \
     --method DELETE \
     "repos/$REPO/issues/$PR_NUMBER/labels"

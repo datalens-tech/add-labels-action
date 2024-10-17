2d710ea82ed50ee1fc1982a309d4035573c6ff94
flukolo4ek <askolo4ek@yandex-team.ru>
2024-10-17 16:09:31 +0300

    fix: add e2e for sinchronize pr with label (#12)

--- src/construct_labels.sh
+++ src/construct_labels.sh
@@ -16,7 +16,7 @@ TYPE_LABELS_PREFIX=$(echo "$CONFIG_DATA" | jq -r '.section_tags.prefix')
 COMPONENT_LABELS_PREFIX=$(echo "$CONFIG_DATA" | jq -r '.component_tags.prefix')
 
 if [[ -n $CURRENT_LABELS ]]; then
-    OTHER_LABELS=$(echo "$CURRENT_LABELS" | grep -Ev "$TYPE_LABELS_PREFIX|$COMPONENT_LABELS_PREFIX" | sort)
+    OTHER_LABELS=$(echo "$CURRENT_LABELS" | grep -Ev "$TYPE_LABELS_PREFIX|$COMPONENT_LABELS_PREFIX" || true | sort)
     CURRENT_LABELS=$(echo "$CURRENT_LABELS" | grep -E "$TYPE_LABELS_PREFIX|$COMPONENT_LABELS_PREFIX" | sort)
 fi
 
